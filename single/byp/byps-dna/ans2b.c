0896a71eeab38e69e78cfde1f406e4eb2a94df8e
Fernando Fiori <fiorifj@gmail.com>
2019-08-27 22:12:43 -0300

    Modify byps to use ans2b-noif

--- single/byp/byps-dna/ans2b.c
+++ single/byp/byps-dna/ans2b.c
@@ -19,28 +19,12 @@ typedef TABTYPE Tab;
 #define CHARTYPE        unsigned char
 #endif
 
-#if 1
-#define movemask_epi8 _mm256_movemask_epi8
-#define cmpeq_epi8 _mm256_cmpeq_epi8
-#define loadu_si _mm256_loadu_si256
-#define mi __m256i
-#define m16 32
-
-#else
-
-#define movemask_epi8 _mm_movemask_epi8
-#define cmpeq_epi8 _mm_cmpeq_epi8
-#define loadu_si _mm_loadu_si128
-#define mi __m128i
-#define m16 32
-
-#endif
-
 #define CHAR_SET (CHAR_MAX-CHAR_MIN+1)
 
 #define LIM 65536
+#define mask (LIM-1)
 
-uint16_t d3[LIM];
+uint8_t d3[LIM];
 
 static struct
 {
@@ -88,29 +72,65 @@ void ans_prep(CHARTYPE *base, register int m, int k, int q)
 #define MAXTEXT 1024
 //~ CHARTYPE text[MAXTEXT];
 
-int ans_exec(CHARTYPE *y, int n, int k, int q)
-{n--;//TODO  careful
-	int i, j, id, matches = 0, m=pat.patlen, nm=n-m;
-	uint32_t t,mask;
-	mi x_ptr, y_ptr;
+int ans_exec_short (CHARTYPE * y, int n, int k, int q)
+{
+    n--;                        //TODO  careful
+    int j, matches = 0, m = pat.patlen, nm = n - m;
+    int t;
+    __m128i x_ptr, y_ptr;
 
-    //~ for(i=0; i<n; i++) text[i]=y[i];
+    x_ptr = _mm_loadu_si128(( __m128i *)(pat.pat));
+    
+    for (j = 0; j <= nm; j++) {
+        y_ptr = _mm_loadu_si128 ((__m128i *) (y + j));
+        t = _mm_movemask_epi8 (_mm_cmpeq_epi8 (x_ptr, y_ptr));
+        matches+=d3[t];
+    }
 
-    mask = (1<<16)-1;
-	//~ for (j = 0; j < m16; j++) text[n+j]=1;
-    //~ y=text;
-
-	x_ptr = loadu_si(( mi *)(pat.pat));
-
-	for (j = 0; j <= nm; j++) {
-		y_ptr = loadu_si(( mi *)(y+j));
-		t = movemask_epi8( cmpeq_epi8(x_ptr, y_ptr) );
-
-		if (d3[t&mask]){
-		    if(m<=16) matches++;
-            else if((m-_mm_popcnt_u32(t))<=k) matches++;
-        }
-	}
-
-	return matches;
+    return matches;
 }
+
+int ans_exec_long (CHARTYPE * y, int n, int k, int q)
+{
+    n--;                        //TODO  careful
+    int j, matches = 0, m = pat.patlen, nm = n - m;
+    int t;
+    __m256i x_ptr, y_ptr;
+
+    x_ptr = _mm256_loadu_si256(( __m256i *)(pat.pat));
+
+    for (j = 0; j <= nm; j++) {
+        y_ptr = _mm256_loadu_si256 ((__m256i *) (y + j));
+        t = _mm256_movemask_epi8 (_mm256_cmpeq_epi8 (x_ptr, y_ptr));
+        //~ if(t<0 || t>256) printf("t %d, n %d, m %d\n",t,n,m);
+        if (d3[t & mask]) {
+            if ((m - _mm_popcnt_u32 (t)) <= k)
+                matches++;
+        }
+    }
+
+    return matches;
+}
+
+//~ int ans_exec(CHARTYPE *y, int n, int k, int q)
+//~ {n--;//TODO  careful
+	//~ int i, j, id, matches = 0, m=pat.patlen, nm=n-m;
+	//~ uint32_t t,mask;
+	//~ mi x_ptr, y_ptr;
+
+    //~ mask = (1<<16)-1;
+
+	//~ x_ptr = loadu_si(( mi *)(pat.pat));
+
+	//~ for (j = 0; j <= nm; j++) {
+		//~ y_ptr = loadu_si(( mi *)(y+j));
+		//~ t = movemask_epi8( cmpeq_epi8(x_ptr, y_ptr) );
+
+		//~ if (d3[t&mask]){
+		    //~ if(m<=16) matches++;
+            //~ else if((m-_mm_popcnt_u32(t))<=k) matches++;
+        //~ }
+	//~ }
+
+	//~ return matches;
+//~ }
