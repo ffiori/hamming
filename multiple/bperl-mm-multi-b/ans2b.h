3c08dc9c46e37f865d0a12806e4fac733fac7baa
Fernando Fiori <fiorifj@gmail.com>
2019-09-06 18:06:20 -0300

    Added MBYPS and MBYPSb distinction

---
+++ multiple/bperl-mm-multi-b/ans2b.h
@@ -0,0 +1,11 @@
+#ifndef ANS2B
+#define ANS2B
+
+#ifndef CHARTYPE
+#define CHARTYPE        unsigned char
+#endif
+
+int ans_exec_short (CHARTYPE * y, int n, int k, int patnow);
+int ans_exec_long (CHARTYPE * y, int n, int k, int patnow);
+
+#endif //ANS2B
---
+++ multiple/bperl-mm-multi-b/main.c
@@ -0,0 +1,139 @@
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#define		NTIME		1000
+
+char buf[5123456];
+char wbuf[33 * 101000];
+long tm[NTIME + 1];
+int nbuf;
+extern int hz;
+int option = -1;
+
+cmp (a, b)
+     long *a, *b;
+{
+    if (*a < *b)
+        return (-1);
+    else
+        return ((*a > *b) ? 1 : 0);
+}
+
+main (argc, argv)
+     char **argv;
+{
+    int c, cnt;
+    int kval = 0;
+    int qval = 1;
+    int npreproc = 1;
+    int nexec = 1;
+    long tp, te, t;
+    long *tmp = tm;
+    int nmatched, len, totlen;
+    long nf, nw;
+    char *s, *ss, *sys = "?";
+    int percent = 0;
+    extern char *optarg;
+    extern int optind;
+
+    while ((c = getopt (argc, argv, "%k:q:e:p:s:o:")) != -1)
+        switch (c) {
+        case 'k':
+            kval = atoi (optarg);
+            break;
+        case 'q':
+            qval = atoi (optarg);
+            break;
+        case 'p':
+            npreproc = atoi (optarg);
+            break;
+        case 'e':
+            nexec = atoi (optarg);
+            break;
+        case 's':
+            sys = optarg;
+            break;
+        case 'o':
+            option = atoi (optarg);
+            break;
+        case '%':
+            percent = 1;
+            break;
+        default:
+            goto usage;
+        }
+    if ((optind >= argc) || (optind + 2 < argc)) {
+      usage:
+        fprintf (stderr, "usage: %s [-k err] [-q wid] [-e nex] [-p npp] [string] file\n", argv[0]);
+        exit (1);
+    }
+    if ((c = open (argv[argc - 1], 0)) < 0) {
+        perror (argv[argc - 1]);
+        exit (1);
+    }
+    nbuf = read (c, buf + 1, sizeof buf - 1025);
+    close (c);
+    if (optind + 2 == argc) {
+        te = strlen (argv[optind]);
+        strcpy (wbuf, argv[optind]);
+        wbuf[te + 1] = 0;
+    }
+    else {
+        s = wbuf;
+
+        while ((c = getchar ()) != EOF)
+            *s++ = (c == '\n') ? 0 : c;
+        totlen = s - wbuf;      //total length of patterns file
+        *s = 0;
+    }
+    tp = te = 0;
+    nf = nw = 0;
+    int Plen = 0;
+
+    for (s = wbuf; *s != 0; ++s)
+        ++Plen;
+    nw = (totlen + 1) / (Plen + 1);     //len+1 in case last pattern finishes in '\0' instead of '\n'
+    for (s = wbuf; *s;) {       //executes preprocessing and search for each pattern.
+        //nw++;
+        /* convert # to \n so we can do multiword matches */
+        ss = s;
+        while (*s) {
+            if (*s == '#')
+                *s = '\n';
+            s++;
+        }
+        len = s - ss;
+        s++;
+
+        //preprocess
+        startclock ();
+        for (c = 0; c < npreproc; c++)
+            prep (ss, totlen, kval, qval);
+        //prep(ss, len, kval, qval); //changed by Fernando
+        tp += stopclock ();
+
+        //search
+        startclock ();
+        for (c = 0; c < nexec; c++) {
+            int i;
+            for(i=0; i<Plen+1; i++) buf[1+nbuf+i]=1;
+            if (cnt = exec (buf + 1, nbuf, kval, qval))
+                nmatched++;
+            nf += cnt;
+        }
+        te += (t = stopclock ());
+
+        if (tmp < &tm[NTIME])
+            *tmp++ = t;
+
+        break;                  //written by Fernando because I want to test multiple pattern matching.
+    }
+    //printf("alg=%s\tnp=%d\tmatch=%d\tp=%.4fs(%d/%d)\te=%.4fs(%d/%d)\n",
+    if (nexec == 0)
+        nexec = 1;
+    if (strlen (argv[0]) > 20)
+        argv[0] = argv[0] + strlen (argv[0]) - 19;
+    printf ("alg=%-20s np=%-5d match=%-10d p=%.5fs e=%.5fs total=%.5fs\n", argv[0], nw, nf / nexec, (tp * 1.0) / (hz * npreproc), (te * 1.0) / (hz * nexec),
+            (tp * 1.0) / (hz * npreproc) + (te * 1.0) / (hz * nexec));
+    exit (0);
+}
